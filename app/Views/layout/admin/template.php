00623666c4a1671682f9a1c19407c46ea3c80dfc
Winartodev <winarto007zing@gmail.com>
2020-12-21 06:39:20 +0700

    Update Template : add JS code
    JS code to create view update and delete in kerusakan

--- app/Views/layout/admin/template.php
+++ app/Views/layout/admin/template.php
@@ -13,6 +13,9 @@
   <link rel="stylesheet" href="<?= base_url(); ?>/assets/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="<?= base_url(); ?>/assets/plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="<?= base_url(); ?>/assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
   <link rel="stylesheet"
     href="<?= base_url(); ?>/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
@@ -119,6 +122,8 @@
   <script src="<?= base_url(); ?>/assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
   <script src="<?= base_url(); ?>/assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
   <script src="<?= base_url(); ?>/assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+  <!-- Select2 -->
+  <script src="<?= base_url(); ?>/assets/plugins/select2/js/select2.full.min.js"></script>
   <script>
   $(document).ready(function () {
     // get Edit Product
@@ -162,6 +167,43 @@
       $('.id_solusi').val(id_solusi); 
       $('.text_id_solusi').text(id_solusi); 
     });
+
+    $('.btn-view-kerusakan').on('click', function () {
+      var id_kerusakan = $(this).data('id_kerusakan');
+      var kerusakan = $(this).data('kerusakan');
+      var id_solusi = $(this).data('id_solusi');
+      var solusi = $(this).data('solusi');
+      $('.id_kerusakan').text(id_kerusakan);
+      $('.kerusakan').text(kerusakan);
+      $('.id_solusi').text(id_solusi);
+      $('.solusi').html(solusi).text();
+    });
+
+    $('.btn-edit-kerusakan').on('click', function () {
+      var id_kerusakan = $(this).data('id_kerusakan');
+      var kerusakan = $(this).data('kerusakan');
+      var id_solusi = $(this).data('id_solusi');
+      $('.id_kerusakan').val(id_kerusakan);
+      $('.kerusakan').val(kerusakan);
+      $('.option_edit_solusi').val(id_solusi).trigger('change');
+    });
+
+    $('.btn-delete-kerusakan').on('click', function () {
+      var id_kerusakan = $(this).data('id_kerusakan');
+      var kerusakan = $(this).data('kerusakan');
+      $('.id_kerusakan').val(id_kerusakan);
+      $('.kerusakan').text(kerusakan);
+    });
+
+    //Initialize Select2 Elements
+    $('.option_tambah_solusi').select2({
+      theme: 'bootstrap4'
+    })
+
+    //Initialize Select2 Elements
+    $('.option_edit_solusi').select2({
+      theme: 'bootstrap4'
+    })
 
     $('#summernote1').summernote();
   });
