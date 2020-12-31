f9367c6b7a2dc214b130fdd7157e5496b9c63f06
Winartodev <winarto007zing@gmail.com>
2020-12-31 09:08:30 +0700

    Update sidebar.php
    Change title text

--- app/Views/layout/admin/sidebar.php
+++ app/Views/layout/admin/sidebar.php
@@ -1,9 +1,8 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="<?= base_url();?>/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="<?= base_url(); ?>/admin" class="brand-link">
+      <p class="brand-text text-center font-weight-light">Expert System <br> Computer Diagnosis</p>
     </a>
 
     <!-- Sidebar -->
